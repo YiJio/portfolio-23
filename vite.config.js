e6108939f3a61f83afdead1d24c0cb9035ae0032
unknown <jy.68171@nevada.unr.edu>
2024-08-25 21:56:29 -0700

    Vite config

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,9 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/portfolio-23/',
   plugins: [vue()],
-  resikve: {
+  resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
     }
